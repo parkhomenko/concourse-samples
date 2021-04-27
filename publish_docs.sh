2bb8626cf4045be0a24f42d69942c697e8c7f7a1
Stanislav Parkhomenko <stanislav.parkhomenko@gmail.com>
2021-04-27 19:15:23 +0300

    changes

--- publish_docs.sh
+++ publish_docs.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
-ls -la ./doc
-
 DOCS_VERSION=$(cat ./docs_version/version.dat)
-echo $DOCS_VERSION
+echo "Current docs version: $DOCS_VERSION"
 
-# git clone fauna-js-repository-docs fauna-js-repository-updated-docs
+git clone fauna-js-repository-docs fauna-js-repository-updated-docs
 
-# cd fauna-js-repository-updated-docs
-# rm -rf *
-# cp ../doc/* ./
+cd fauna-js-repository-updated-docs
+rm -rf *
+cp -R ../doc/* ./
 
-# git add -A
-# git commit -m "Update docs to version: $DOCS_VERSION"
+git add -A
+git commit -m "Update docs to version: $DOCS_VERSION"
