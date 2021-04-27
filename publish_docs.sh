5ee57eb591a774a569e271b6f97d29bfc5cbe128
Stanislav Parkhomenko <stanislav.parkhomenko@gmail.com>
2021-04-27 18:11:01 +0300

    changes

--- publish_docs.sh
+++ publish_docs.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
 ls -la ./doc
+
+git clone fauna-js-repository-docs fauna-js-repository-updated-docs
+cd fauna-js-repository-updated-docs
+echo "In original repo"
+ls
+rm -rf *
+echo "After removal of all the files"
+ls
+cp ../doc/* ./
+echo "After copy of new files"
+ls
+git status
