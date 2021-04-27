18362f7d738e4626cc9402865b6ced9380026696
Stanislav Parkhomenko <stanislav.parkhomenko@gmail.com>
2021-04-27 13:16:14 +0300

    changes

--- publish.sh
+++ publish.sh
@@ -10,8 +10,7 @@ npm run browserify
 npm run browserify-min
 
 PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
-#NPM_LATEST_VERSION=$(npm view faunadb version)
-NPM_LATEST_VERSION=4.3.0
+NPM_LATEST_VERSION=$(npm view faunadb version)
 echo "Current package version: $PACKAGE_VERSION"
 echo "Latest version in npm: $NPM_LATEST_VERSION"
 
